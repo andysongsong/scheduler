d9e24987b49f8d5a93934c401425d838193a9127
andysongsong <sxj930916@gmail.com>
2021-08-31 18:16:34 -0700

    add state to daylist

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,32 +1,7 @@
-import React from "react";
 import DayList from "./DayList";
+import React, { useState } from "react";
 
 import "components/Application.scss";
-
-export default function Application(props) {
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
-}
 
 const days = [
   {
@@ -45,3 +20,31 @@ const days = [
     spots: 0,
   },
 ];
+
+export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+  return (
+    <main className="layout">
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+      </section>
+      <section className="schedule">
+        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      </section>
+    </main>
+  );
+}
